b48d18afeeb0006945ab6f64c8276aa6541d7a2a
bigdawgliana <liana.ortiz25@compscihigh.org>
2024-11-13 18:05:52 +0000

    I need support with my async function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+let getArtistURL = "https://api.spotify.com/v1/artists/2EMAnMvWE2eb56ToJVfCWs/top-tracks";
 
+
+const artist = {
+    id: "2EMAnMvWE2eb56ToJVfCWs",
+};
+
+async function getArtist(artistData){
+
+    const options = {
+        method: 'GET',
+          headers: {
+            Authorization: "Bearer ",
+        }
+
+    }
+}
